c2179a8f6dbafbd98ec5418571302b1cbb8f5754
Wutzuket <maximka.grenz@yahoo.com>
2025-08-27 19:05:29 +0200

    added craftig recipes/added ponder scenes

--- src/main/java/de/wutzuket/create_overdrive/index/CPAPonders.java
+++ src/main/java/de/wutzuket/create_overdrive/index/CPAPonders.java
@@ -17,11 +17,19 @@ public class CPAPonders {
         PonderTagRegistrationHelper<RegistryEntry<?,?>> HELPER = helper.withKeyFunction(RegistryEntry::getId);
         HELPER.addToTag(AllCreatePonderTags.KINETIC_APPLIANCES).add(CPABlocks.PARTICLE_ACCELERATOR_CORE);
         HELPER.addToTag(AllCreatePonderTags.KINETIC_APPLIANCES).add(CPABlocks.IONATOR);
+        HELPER.addToTag(AllCreatePonderTags.KINETIC_SOURCES).add(CPABlocks.FUSION_REACTOR_CORE);
     }
 
     public static void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
         PonderSceneRegistrationHelper<ItemProviderEntry<?,?>> HELPER = helper.withKeyFunction(RegistryEntry::getId);
+
         HELPER.addStoryBoard(CPABlocks.PARTICLE_ACCELERATOR_CORE, "particle_accelerator_core",
+                CPAPonderScenes::particleAcceleratorCoreScene);
+
+        HELPER.addStoryBoard(CPABlocks.ACCELERATOR_INPUT, "particle_accelerator_core",
+                CPAPonderScenes::particleAcceleratorCoreScene);
+
+        HELPER.addStoryBoard(CPABlocks.ACCELERATOR_OUTPUT, "particle_accelerator_core",
                 CPAPonderScenes::particleAcceleratorCoreScene);
 
         HELPER.addStoryBoard(CPABlocks.IONATOR, "ionator",
@@ -31,6 +39,12 @@ public class CPAPonders {
                 CPAPonderScenes::ionize);
 
         HELPER.addStoryBoard(CPABlocks.FUSION_REACTOR_CORE, "fusion_reactor",
+                CPAPonderScenes::fusionReactorCore);
+
+        HELPER.addStoryBoard(CPABlocks.FUSION_REACTOR_CASING, "fusion_reactor",
+                CPAPonderScenes::fusionReactorCore);
+
+        HELPER.addStoryBoard(CPABlocks.FUSION_REACTOR_INPUT, "fusion_reactor",
                 CPAPonderScenes::fusionReactorCore);
     }
 }
